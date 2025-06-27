5347d1ec397319773a5da13593ed2a424a3a5c3f
omusakhulu <nduggajoseph@gmail.com>
2025-06-27 11:32:46 +0300

    26

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,7 @@ import { prisma } from '@/lib/db'
 import bcrypt from 'bcryptjs'
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
